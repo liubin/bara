a6a1ad085abfc67559eae1ec3c289ea7e891ea48
bin liu <liubin0329@gmail.com>
2015-12-10 18:07:35 +0800

    add config description for Docker connection info

--- config/initializers/docker.rb
+++ config/initializers/docker.rb
@@ -1,3 +1,6 @@
+
+# if you want to connect a Docker daemon not on loclhost
+# you should do the following settings.
 Docker.options = {
   client_cert: File.join(ENV['CERT_PATH'], 'cert.pem'),
   client_key: File.join(ENV['CERT_PATH'], 'key.pem'),
@@ -8,3 +11,7 @@ Docker.options = {
 Docker.url='tcp://192.168.99.100:2376'
 
 
+# if you want to use an Unix socket to connect docker daemon
+# you should set Docker.url to unix:///var/run/docker.sock
+
+# Docker.url='unix:///var/run/docker.sock'
